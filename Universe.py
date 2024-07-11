b7e5e7ce76d98de4db7745777cec56d5da4bb68f
Juan Bavaresco <juan@faculty.masterschool.com>
2024-07-11 13:18:43 +0200

    Implementing inheritance with the CelestialBody class and Polymorphism through the get_host() method

--- Universe.py
+++ Universe.py
@@ -1,46 +1,54 @@
 from scipy import constants
+from abc import ABC, abstractmethod
+
+
+class CelestialBody(ABC):
+    def __init__(self, name, radius_in_m, mass_in_kg, mean_temperature, age):
+        self.name = name
+        self.radius_in_m = radius_in_m
+        self.mass_in_kg = mass_in_kg
+        self.mean_temperature = mean_temperature
+        self.age = age
+
+    def gravitational_acceleration(self):
+        return (constants.gravitational_constant * self.mass_in_kg) / (self.radius_in_m ** 2)
+
+    # Abstract method that each subclass will have to implement
+    @abstractmethod
+    def get_host(self):
+        pass
 
 
 # Class for defining a Planet
-class Planet:
-    def __init__(self, name, radius_in_m, mass_in_kg, mean_temperature_in_celsius, age_in_years, is_habitable, host_star=None):
-        self.name = name
-        self.radius_in_m = radius_in_m
-        self.mass_in_kg = mass_in_kg
-        self.mean_temperature_in_celsius = mean_temperature_in_celsius
-        self.age_in_years = age_in_years
+class Planet(CelestialBody):
+    def __init__(self, name, radius_in_m, mass_in_kg, mean_temperature_in_celsius, age_in_years, is_habitable,
+                 host_star=None):
+        super().__init__(name, radius_in_m, mass_in_kg, mean_temperature_in_celsius, age_in_years)
         self.is_habitable = is_habitable
         self.host_star = host_star
 
-    # Method for calculating the gravitational acceleration
-    # Notice how the method is the same in all classes
-    def gravitational_acceleration(self):
-        return (constants.gravitational_constant * self.mass_in_kg) / (self.radius_in_m ** 2)
+    def get_host(self):
+        return self.host_star
 
 
 # Class for defining a Star
-class Star:
-    def __init__(self, name, radius_in_m, mass_in_kg, mean_temperature_in_celsius, age_in_years, surrounding_planets=None):
-        self.name = name
-        self.radius_in_m = radius_in_m
-        self.mass_in_kg = mass_in_kg
-        self.mean_temperature_in_celsius = mean_temperature_in_celsius
-        self.age_in_years = age_in_years
+class Star(CelestialBody):
+    def __init__(self, name, radius_in_m, mass_in_kg, mean_temperature_in_celsius, age_in_years,
+                 surrounding_planets=None, host_galaxy=None):
+        super().__init__(name, radius_in_m, mass_in_kg, mean_temperature_in_celsius, age_in_years)
         self.surrounding_planets = surrounding_planets
+        self.host_galaxy = host_galaxy
 
-    def gravitational_acceleration(self):
-        return (constants.gravitational_constant * self.mass_in_kg) / (self.radius_in_m ** 2)
+    def get_host(self):
+        return self.host_galaxy
 
 
 # Class for defining a Moon
-class Moon:
+class Moon(CelestialBody):
     def __init__(self, name, radius_in_m, mass_in_kg, mean_temperature_in_celsius, age_in_years, host_planet=None):
-        self.name = name
-        self.radius_in_m = radius_in_m
-        self.mass_in_kg = mass_in_kg
-        self.mean_temperature_in_celsius = mean_temperature_in_celsius
-        self.age_in_years = age_in_years
+        super().__init__(name, radius_in_m, mass_in_kg, mean_temperature_in_celsius, age_in_years)
         self.host_planet = host_planet
 
-    def gravitational_acceleration(self):
-        return (constants.gravitational_constant * self.mass_in_kg) / (self.radius_in_m ** 2)
+    def get_host(self):
+        return self.host_planet
+
--- main.py
+++ main.py
@@ -2,12 +2,19 @@ from Universe import *
 
 
 def main():
-    earth = Planet("earth", 6.371e6, 5.972e24, 15, 4.54e9, True)
 
-    moon = Moon("moon", 1.7371e6, 7.342e22, -20, 4.53e9, earth)
+    sun = Star("Sun", 6.9634e8, 1.989e30, 5505, 4.6e9)
+    earth = Planet("Earth", 6.371e6, 5.972e24, 15, 4.54e9, True, sun)
+    moon = Moon("Moon", 1.7371e6, 7.342e22, -20, 4.53e9, earth)
 
-    print(moon)
-    print(earth)
+    print(f"{earth.name} class: {earth}")
+    print(f"{earth.name} host: {earth.get_host()}\n")
+
+    print(f"{earth.name} class: {moon}")
+    print(f"{sun.name} host: {moon.get_host()}\n")
+
+    print(f"{sun.name} class: {sun}")
+    print(f"{sun.name} host: {sun.get_host()}\n")
 
 
 if __name__ == '__main__':
